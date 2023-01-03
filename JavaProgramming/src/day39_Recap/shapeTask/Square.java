ba037b54c1ce79ede68af4698c974265861f25ef
KMN <kuwwatt@gmail.com>
2023-01-03 21:28:18 +0100

    day39_Recap_Shape

--- JavaProgramming/src/day39_Recap/shapeTask/Square.java
+++ JavaProgramming/src/day39_Recap/shapeTask/Square.java
@@ -1,9 +1,27 @@
 package day39_Recap.shapeTask;
 
-public class Square {
+public class Square extends Shape{
 
+     private double side;
 
-/*
+    public Square(double side) {
+        super("Square");
+        setSide(side);
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        if (side<=0){
+            System.err.println("Invalid Side");
+            System.exit(1);
+        }
+        this.side = side;
+    }
+
+     /*
 Square extends Shape:
 variables:
 side;
