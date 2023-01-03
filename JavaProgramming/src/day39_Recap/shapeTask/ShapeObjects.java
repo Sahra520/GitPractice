3c8def51e1194a04f626bd6bc2b435a82a979033
KMN <kuwwatt@gmail.com>
2023-01-03 23:09:27 +0100

    day39_Recap_Shape

---
+++ JavaProgramming/src/day39_Recap/shapeTask/ShapeObjects.java
@@ -0,0 +1,18 @@
+package day39_Recap.shapeTask;
+
+public class ShapeObjects {
+
+    public static void main(String[] args) {
+
+        Circle circle = new Circle(10);
+
+        System.out.println(circle);
+
+        Rectangle rectangle = new Rectangle(23.5, 34.6);
+        System.out.println(rectangle);
+
+        Square square = new Square(4.8);
+        System.out.println(square);
+
+    }
+}
