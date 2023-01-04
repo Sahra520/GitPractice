7fb3eac28ebaf325d0a85bffc70891816cf0e627
KMN <kuwwatt@gmail.com>
2023-01-04 14:08:42 +0100

    day39_Recap_Shape

--- JavaProgramming/src/day39_Recap/shapeTask/ShapeObjects.java
+++ JavaProgramming/src/day39_Recap/shapeTask/ShapeObjects.java
@@ -1,15 +1,28 @@
 package day39_Recap.shapeTask;
 
+import java.util.concurrent.Callable;
+
 public class ShapeObjects {
 
     public static void main(String[] args) {
 
         Circle circle = new Circle(10);
-
         System.out.println(circle);
+
+        Circle circle1 = new Circle(12);
+        circle1.getName();
+        circle1.setRadius(16);
+        System.out.println("");
+        System.out.println(circle1.getRadius());
+        System.out.println("");
+        System.out.println(circle1);
+
+        System.out.println("");
 
         Rectangle rectangle = new Rectangle(23.5, 34.6);
         System.out.println(rectangle);
+
+        System.out.println("");
 
         Square square = new Square(4.8);
         System.out.println(square);
