0755ea0bfb1a9c9c2fa7d64865c8cf8a6379184a
KMN <kuwwatt@gmail.com>
2023-01-05 13:24:40 +0100

    day40_FinalKeyboard

---
+++ JavaProgramming/src/day40_FinalKeyboard/Test.java
@@ -0,0 +1,19 @@
+package day40_FinalKeyboard;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        final int number = 100;
+        //number = 200;//compile error
+
+
+
+    }
+    public final static double pi = 3.14;
+
+    public final String dateOfBirth = "23/02/1985";
+
+    public final String firstLanguage = "turkmen";
+
+}
