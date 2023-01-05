7942d2c8c563b4df2dd6dcce2a7f16bcfd7dad46
KMN <kuwwatt@gmail.com>
2023-01-05 14:16:51 +0100

    day40_FinalKeyboard

---
+++ JavaProgramming/src/day40_FinalKeyboard/FinalVariable.java
@@ -0,0 +1,34 @@
+package day40_FinalKeyboard;
+
+public class FinalVariable {
+
+    final String birthDay; //without constructor giving compile error
+
+    final static String name;
+    static {
+        name = "Batch25";
+    }
+
+    public FinalVariable(String birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public static void main(String[] args) {
+
+        final double pi = 3.14;
+
+        System.out.println("  ");
+
+        FinalVariable obj = new FinalVariable("May/21");
+
+        //obj.birthDay = "June/01"; //final variables can not be reassigned
+
+        System.out.println(obj.birthDay);
+
+        System.out.println(" ");
+
+        System.out.println(name);
+        //FinalVariable.name = "Python";//we can not reassign final variable
+
+    }
+}
