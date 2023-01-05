85038f4331fc0d6a960aec4814056ca2b9041ee1
KMN <kuwwatt@gmail.com>
2023-01-05 15:40:27 +0100

    day37_Phone

--- JavaProgramming/src/day37_Inheritance_SuperKeyword/phoneTask/Iphone.java
+++ JavaProgramming/src/day37_Inheritance_SuperKeyword/phoneTask/Iphone.java
@@ -8,12 +8,12 @@ public final class Iphone extends Phone {
     }
 
 
-    public void faceTime(long phoneNumber){
+    public final void faceTime(long phoneNumber){
         System.out.println(brand + " " + model + " is having a Face Time with phone number" + phoneNumber);
     }
 
 
-    public void faceTime(String email){
+    public final void faceTime(String email){
         System.out.println(brand +" " + model + " is having a Face Time with email" + email);
     }
 
---
+++ JavaProgramming/src/day40_FinalKeyboard/Cat.java
@@ -0,0 +1,17 @@
+package day40_FinalKeyboard;
+
+public final class Cat extends Animal {
+
+
+    public Cat(String name, String bread, char gender, String color, String size, int age) {
+        super(name, bread, gender, color, size, age);
+    }
+
+
+
+
+
+
+
+
+}
