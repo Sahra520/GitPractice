a8bc699683ae85fd67d13a567e004984529f58a9
KMN <kuwwatt@gmail.com>
2023-01-05 13:18:05 +0100

    day40_FinalKeyboard

--- JavaProgramming/src/day40_FinalKeyboard/ProtectedAccessModifier.java
+++ JavaProgramming/src/day40_FinalKeyboard/ProtectedAccessModifier.java
@@ -7,7 +7,14 @@ public class ProtectedAccessModifier {
     protected static String name2 = "Wooden Spoon"; // protected
 
 
+    static void method1(){
 
+    }
+
+
+    protected static void method2(){
+
+    }
 
 
 
---
+++ JavaProgramming/src/utilities/AccessModifiers_Test4.java
@@ -0,0 +1,15 @@
+package utilities;
+
+import day40_FinalKeyboard.ProtectedAccessModifier;
+
+public class AccessModifiers_Test4 extends ProtectedAccessModifier {
+
+    public static void main(String[] args) {
+
+       //System.out.println(AccessModifiers_Test4.name1);//default is not visible outside the package
+        System.out.println(AccessModifiers_Test4.name2);//protected is visible outside the package IN subclass only
+
+        //AccessModifiers_Test4.method1();//default is not visible outside the package
+        AccessModifiers_Test4.method2();//protected is visible outside the package IN subclass only
+    }
+}
