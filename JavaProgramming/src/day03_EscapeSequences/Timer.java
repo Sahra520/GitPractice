fe12153ee8e8d5029cafa0869c8da91984b82071
KMN <kuwwatt@gmail.com>
2023-01-13 15:56:21 +0100

    Timer.java

--- JavaProgramming/src/day03_EscapeSequences/Timer.java
+++ JavaProgramming/src/day03_EscapeSequences/Timer.java
@@ -14,7 +14,7 @@ public class Timer {
             }
             for (int z=59; z>=0; --z){
                 System.out.println("\r"+(i-1)+"minutes and " +z+ "seconds left");
-                Thread.sleep(minutes);
+                Thread.sleep(1000);
             }
         }
         System.err.println("\n\n");
