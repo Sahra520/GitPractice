cc463d7c6189b294393dda295b2e05db8157b9d1
KMN <kuwwatt@gmail.com>
2023-01-21 22:38:01 +0100

    day43_OOPAbstractionIntro_abstractClass_abstractMethod; Employee.java

---
+++ JavaProgramming/src/day43_OOPAbstractionIntro_abstractClass_abstractMethod/Employee/Employee.java
@@ -0,0 +1,69 @@
+package day43_OOPAbstractionIntro_abstractClass_abstractMethod.Employee;
+
+public abstract class Employee extends Person {
+
+    private final int id;
+    private String jobTitle;
+    private double salary;
+
+    public Employee(String name, int age, char gender, int id, String jobTitle, double salary) {
+        super(name, age, gender);
+        this.id = id;
+        this.jobTitle = jobTitle;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public abstract void work();
+
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + getName() + '\'' +
+                ", age=" + getAge() +
+                ", gender=" + getGender() +
+                ", id=" + id +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
+/*
+Employee task:
+abstract Person:
+name, age, gender(final)
+eat();
+sleep();
+abstract Employee extends Person:
+ id(final), jobTitle, salary...
+work();
+Tester
+work(): testing
+Developer
+work(): developing
+Teacher
+work(): teaching
+Driver
+work(): driving
+...
+ */
