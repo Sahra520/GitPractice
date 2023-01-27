af5b10543d284b3765b627b07345f964278875dc
KMN <kuwwatt@gmail.com>
2023-01-27 21:44:24 +0100

    day44_OOPAbstractionContinue_Interface.animalTask; WildAnimal.java

---
+++ JavaProgramming/src/day44_OOPAbstractionContinue_Interface/animalTask/WildAnimal.java
@@ -0,0 +1,10 @@
+package day44_OOPAbstractionContinue_Interface.animalTask;
+
+public interface WildAnimal {
+
+    boolean isWild = true; // static and final by default
+
+    void hunt(); //public and abstract by default
+
+
+}
